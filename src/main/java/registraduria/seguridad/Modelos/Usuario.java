d4baa9fc5058d2f91e7a1d2e5c34304f8a120230
RubenCameroAmador <37782994+RubenCameroAmador@users.noreply.github.com>
2022-09-17 11:46:46 -0500

    Creción de la parte del usuario, V1, sin mensajes

---
+++ src/main/java/registraduria/seguridad/Modelos/Usuario.java
@@ -0,0 +1,68 @@
+package registraduria.seguridad.Modelos;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Data
+@Document()
+
+public class Usuario {
+    @Id
+    private String _id;
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasena;
+
+    public Usuario(String cedula, String nombre, String apellido, String correo, String contrasena) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
---
+++ src/main/java/registraduria/seguridad/Repositorios/RepositorioUsuario.java
@@ -0,0 +1,7 @@
+package registraduria.seguridad.Repositorios;
+
+import registraduria.seguridad.Modelos.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RepositorioUsuario extends MongoRepository<Usuario,String> {
+}
